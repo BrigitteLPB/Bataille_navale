29afc634eb69527ef62ab4024ef59fe014f24de1
BrigitteLPB <guillemaud.theo@gmail.com>
2022-02-09 01:29:27 +0100

    release 1.0

--- .vscode/move_assets.windows.cmd
+++
@@ -1,3 +0,0 @@
-robocopy .\assets .\build\assets **.* /s /j /purge
-if %ERRORLEVEL% GEQ 9 exit /b 1
-exit /b 0
--- .vscode/move_src.windows.cmd
+++
@@ -1,4 +0,0 @@
-robocopy .\src .\build\ **.py /s /j /purge
-if %ERRORLEVEL% GEQ 9 exit /b 1
-
-copy /V /Y .env .\build\
